494b12b16a3a042d7b332f0c590f354139203a95
jeongbe <101166792+jeongbe@users.noreply.github.com>
2024-01-08 09:35:04 +0900

    시작

---
+++ JumpExpo/src/main/java/com/example/JumpExpo/JumpExpoApplication.java
@@ -0,0 +1,13 @@
+package com.example.JumpExpo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JumpExpoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JumpExpoApplication.class, args);
+	}
+
+}
---
+++ JumpExpo/src/test/java/com/example/JumpExpo/JumpExpoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JumpExpo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JumpExpoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
