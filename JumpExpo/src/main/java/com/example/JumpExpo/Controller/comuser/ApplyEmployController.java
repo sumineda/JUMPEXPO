def34fba3d690bcc7592f4e04ec80d3cdfc5940a
Echae <91455648+Parkeun12@users.noreply.github.com>
2024-01-09 16:22:49 +0900

    채용 공고 신청 Controller

---
+++ JumpExpo/src/main/java/com/example/JumpExpo/Controller/comuser/ApplyEmployController.java
@@ -0,0 +1,41 @@
+package com.example.JumpExpo.Controller.comuser;
+
+import com.example.JumpExpo.DTO.comuser.EmployForm;
+import com.example.JumpExpo.Entity.comuser.ApplyEmploy;
+import com.example.JumpExpo.Repository.comuser.ApplyEmployRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+//2024.01.08 박은채 채용 공고 신청 Controller
+//채용 공고 신청 Controller
+@Slf4j
+@Controller
+@RequestMapping("/comuser")
+public class ApplyEmployController {
+
+    @Autowired
+    ApplyEmployRepository applyEmployRepository;
+
+    //채용 공고 신청 페이지
+    @GetMapping("/insert/employ")
+    public String insertEmploy(){
+
+        return "comuser/applyemploy/ApplyEmploy";
+    }
+
+    @PostMapping("/save/employ")
+    public String saveEmploy(EmployForm form){
+
+        log.info(form.toString());
+
+        ApplyEmploy data = form.toEnttiy();
+        // 디폴트 비승인 값 0 고정
+        data.setRecog_check(0);
+        ApplyEmploy save = applyEmployRepository.save(data);
+        log.info(save.toString());
+
+        return "";
+    }
+}
