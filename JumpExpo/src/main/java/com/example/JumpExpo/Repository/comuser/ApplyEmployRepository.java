39d1337eea7c1ba53f5546043d6ffe201f1b0707
Echae <91455648+Parkeun12@users.noreply.github.com>
2024-01-09 16:05:09 +0900

    채용 공고 심사 리스트 쿼리문 작성
    심사중 0 심사완료 1로 나누어서 쿼리문 작성

--- JumpExpo/src/main/java/com/example/JumpExpo/Repository/comuser/ApplyEmployRepository.java
+++ JumpExpo/src/main/java/com/example/JumpExpo/Repository/comuser/ApplyEmployRepository.java
@@ -1,10 +1,30 @@
 package com.example.JumpExpo.Repository.comuser;
 
-import com.example.JumpExpo.Entity.admin.ScheduleInsert;
 import com.example.JumpExpo.Entity.comuser.ApplyEmploy;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
+//2024.01.09 박은채
+//채용 공고 심사 Repository
 @Repository
 public interface ApplyEmployRepository  extends JpaRepository<ApplyEmploy, Integer> {
+
+//    @Query(value = "select *\n" +
+//            "from apply_employ" +
+//            "where recog_check = 0", nativeQuery = true)
+//    ArrayList<ApplyEmploy> AllreqEmployList();
+
+    @Query(value = "SELECT *\n" +
+            "FROM apply_employ\n" +
+            "WHERE recog_check = 0;", nativeQuery = true)
+        ArrayList<ApplyEmploy> AllreqEmployList();
+
+    @Query(value = "select *\n" +
+            "from apply_employ\n" +
+            "where recog_check = 1", nativeQuery = true)
+    ArrayList<ApplyEmploy> AlldisreqEmployList();
 }
